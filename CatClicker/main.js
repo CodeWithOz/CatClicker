5724324137cf2f577b4cdcfeee606eb86683acf9
Uche Ozoemena <ucheozoemena@gmail.com>
2018-09-29 07:31:01 +0100

    Programmatically add cat names

--- CatClicker/main.js
+++ CatClicker/main.js
@@ -1,3 +1,10 @@
+// give the cats names
+const cats = [...document.querySelectorAll('img')],
+  catNames = ['Floor cat', 'Stunned cat'];
+cats.forEach((cat, index) => {
+  cat.previousElementSibling.textContent = catNames[index];
+});
+
 // track clicks on the image
 const imageDiv = document.querySelector('.cat-pics');
 imageDiv.addEventListener('click', event => {
