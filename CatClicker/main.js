860b8ea027c7996ae389564b38d5a290e71b8c6e
Uche Ozoemena <ucheozoemena@gmail.com>
2018-10-09 07:06:44 +0100

    Move loadCat to the octopus

--- CatClicker/main.js
+++ CatClicker/main.js
@@ -68,7 +68,9 @@ const octopus = (() => {
     init() {
       const cats = data.getAllCats();
       view.renderSidebar(cats);
-    }
+    },
+
+    loadCat(event) {}
   };
 })();
 
@@ -94,30 +96,6 @@ displayArea.addEventListener('click', event => {
 // display cat when its name is clicked in sidebar
 const imageDiv = document.querySelector('.cat-pic');
 sidebar.addEventListener('click', loadCat);
-
-function loadCat(event) {
-  const { target } = event;
-
-  // exit if click is not from the list item anchor tag
-  if (target.tagName !== 'A') return;
-
-  event.preventDefault();
-
-  // hide the menu
-  sidebar.classList.add('sidebar-hidden');
-
-  // get the parent that contains the necessary info
-  const cat = target.parentElement;
-
-  // populate the main image display
-  const [title, image, countContainer] = [...imageDiv.children[0].children];
-  title.textContent = cat.dataset.name;
-  image.setAttribute('src', cat.dataset.src);
-  countContainer.children[0].textContent = cat.dataset.count;
-
-  // this saves a reference to this cat's matching sidebar item
-  image.dataset.index = cat.dataset.index;
-}
 
 // show the first cat on page load
 sidebarCats[0].children[0].click();
