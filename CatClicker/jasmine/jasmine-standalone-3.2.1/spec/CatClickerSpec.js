1861305750a39a7f58335d2ee1f161607d5851d1
Uche Ozoemena <ucheozoemena@gmail.com>
2018-10-09 06:06:01 +0100

    Refactor tests for sidebar items

--- CatClicker/jasmine/jasmine-standalone-3.2.1/spec/CatClickerSpec.js
+++ CatClicker/jasmine/jasmine-standalone-3.2.1/spec/CatClickerSpec.js
@@ -195,54 +195,28 @@ describe('The sidebar', () => {
  */
 describe('Items on the cat list', () => {
   const sidebar = document.querySelector('.cats-menu'),
-    cats = sidebar.querySelectorAll('li');
+    cats = [...sidebar.querySelectorAll('li')];
 
   they('add up to a total of 5', () => {
     expect(cats.length).toEqual(5);
   });
 
-  they('each have a name property', () => {
+  they('each have an index property', () => {
     cats.forEach(cat => {
-      expect(cat.dataset.name).toBeDefined();
-
-      // must be a string with length > 0
-      expect(cat.dataset.name).toMatch(/.+/);
-    });
-  });
-
-  they('each have a src property', () => {
-    cats.forEach(cat => {
-      expect(cat.dataset.src).toBeDefined();
-
-      // must be a string with length > 0
-      expect(cat.dataset.src).toMatch(/.+/);
-    });
-  });
-
-  they('each have a count property', () => {
-    cats.forEach(cat => {
-      expect(cat.dataset.count).toBeDefined();
+      const innerAnchorTag = cat.children[0];
+      expect(innerAnchorTag.dataset.index).toBeDefined();
 
       // must be a non-NaN number
-      expect(typeof Number(cat.dataset.count)).toBe('number');
-      expect(Number(cat.dataset.count)).not.toBeNaN();
-    });
-  });
-
-  they('each have a index property', () => {
-    cats.forEach(cat => {
-      expect(cat.dataset.index).toBeDefined();
-
-      // must be a non-NaN number
-      expect(typeof Number(cat.dataset.index)).toBe('number');
-      expect(Number(cat.dataset.index)).not.toBeNaN();
+      const index = Number(innerAnchorTag.dataset.index);
+      expect(typeof index).toBe('number');
+      expect(index).not.toBeNaN();
     });
   });
 
   they(`each display the cat's name`, () => {
     cats.forEach(cat => {
-      const clickableArea = cat.children[0];
-      expect(clickableArea.textContent).toEqual(cat.dataset.name);
+      const innerAnchorTag = cat.children[0];
+      expect(innerAnchorTag.textContent).toMatch(/.+/);
     });
   });
 });
